c9c6a2634dc219ee2f4f7ee74d88237841b2df92
David Tsui <david@fq2-wireless-pittnet-47-170.wireless.pitt.edu>
2016-02-13 14:30:10 -0500

    comments

--- CoffeeMaker_QA/DoorDescription.java
+++ CoffeeMaker_QA/DoorDescription.java
@@ -1,16 +1,25 @@
+/**
+ * Door class for north and south door in Coffeemaker
+ * @author David Neiman and David Tsui
+ */
 public class DoorDescription {
 	String description;
 	
 	public DoorDescription (String name) {
 		this.description = name;
 	}
-	
+	/**
+	 * @param name set the description of the door
+	 * @return boolean returns true if not null
+	 */
 	public boolean setDescription (String name) {
 		if (name == null) return false;
 		this.description = name;
 		return true;
 	}
-	
+	/**
+	 * @return the description of the door
+	 */
 	public String getDescription () {
 		return description;
 	}
